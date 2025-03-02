48819c321023c051c363bcc843ad2fe878d9186f
Wojciech Macek <wojciech.macek@gmail.com>
2025-03-02 07:35:44 +0100

    do not turn off light sleep
    Light Sleep once enabled should never be turned off.
    Otherwise, the ESP32 is unstable and might crash.
    
    To mitigate light-sleep "disable", create a lock which cannot sleep.
    This forces the ESP-IDF not to enter light-sleep when the lock is
    being held.

--- components/light_sleep_switch/light_sleep_switch.cpp
+++ components/light_sleep_switch/light_sleep_switch.cpp
@@ -11,24 +11,21 @@ namespace light_sleep_switch {
 static const char *TAG = "light_sleep_switch.switch";
 
 void LightSleepSwitch::setup() {
-
+    esp_pm_lock_create(ESP_PM_NO_LIGHT_SLEEP, 0, "early", &light_sleep_lock);
+    esp_pm_lock_acquire(light_sleep_lock);
+    esp_pm_config_t pm_conf {
+        .max_freq_mhz = 80,
+        .min_freq_mhz = 40,
+        .light_sleep_enable = true,
+    };
+    esp_pm_configure(&pm_conf);
 }
 
 void LightSleepSwitch::write_state(bool state) {
     if (state == true) {
-        esp_pm_config_t pm_conf {
-            .max_freq_mhz = 80,
-            .min_freq_mhz = 40,
-            .light_sleep_enable = true,
-          };
-          esp_pm_configure(&pm_conf);
+	esp_pm_lock_release(light_sleep_lock);
     } else {
-        esp_pm_config_t pm_conf {
-            .max_freq_mhz = 240,
-            .min_freq_mhz = 80,
-            .light_sleep_enable = false,
-          };
-          esp_pm_configure(&pm_conf);
+	esp_pm_lock_acquire(light_sleep_lock);
     }
 }
 
--- components/light_sleep_switch/light_sleep_switch.h
+++ components/light_sleep_switch/light_sleep_switch.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <esp_pm.h>
+#include <esp_sleep.h>
+
 #include "esphome/core/component.h"
 #include "esphome/components/switch/switch.h"
 
@@ -11,6 +14,8 @@ class LightSleepSwitch : public switch_::Switch, public Component {
   void setup() override;
   void write_state(bool state) override;
   void dump_config() override;
+ private:
+  esp_pm_lock_handle_t light_sleep_lock;
 };
 
 } //namespace light_sleep_switch
